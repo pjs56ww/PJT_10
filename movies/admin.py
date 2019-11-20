ef816af46480d573a0021baf482f4c184eacaeab
joohongkim1 <wnghd9938@naver.com>
2019-11-20 15:50:53 +0900

    Finish DB Modeling

---
+++ movies/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Genre, Movie, Review
+
+admin.site.register(Genre)
+admin.site.register(Movie)
+admin.site.register(Review)
+# Register your models h.
